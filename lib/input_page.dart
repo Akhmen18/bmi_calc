4754e2f2576ae34fc4fd76e317b16010c83fb40c
Akhil Menon <akh180599@gmail.com>
2021-03-03 20:46:35 +0530

    Add Immutability to the class variable

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -55,7 +55,7 @@ class _InputPageState extends State<InputPage> {
 }
 
 class ReusableCard extends StatelessWidget {
-  Color colour;
+  final Color colour;
 
   ReusableCard({@required this.colour}) {
     // My Constructor.
